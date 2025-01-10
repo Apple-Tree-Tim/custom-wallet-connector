354f77af9955f0d814e2856b5958375d32e8a30d
Smart Dev <TimBeamon11910@gmail.com>
2025-01-10 15:17:06 -0500

    update file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,50 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
+import { createConfig, http, WagmiProvider } from 'wagmi'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import {
+  rainbowWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { CustomWallet } from "../wallets/CustomWallet";
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from "wagmi/chains";
+import { createClient } from 'viem'
 
-import { config } from '../wagmi';
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [CustomWallet],
+    },
+    {
+      groupName: "Others",
+      wallets: [rainbowWallet, walletConnectWallet],
+    },
+  ],
+  {
+    appName: "My RainbowKit App",
+    projectId: "YOUR_PROJECT_ID",
+  }
+);
+
+const config = createConfig({
+  chains: [arbitrum, base, mainnet, optimism, polygon, sepolia],
+  connectors,
+  client({ chain }) {
+    return createClient({ chain, transport: http() })
+  },
+});
+
 
 const client = new QueryClient();
 
